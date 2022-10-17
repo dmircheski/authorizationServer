001345cb4eab28d389ccb3938d7d31259ad3e35b
dmircheski <deni.mirceski@t.mk>
2022-10-17 20:30:24 +0200

    database url changed to deployed one

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoDbUrl: 'mongodb://localhost:27017/Users'
+    mongoDbUrl: 'mongodb+srv://' + process.env.ATLAS_DB_USER + ':' + process.env.ATLAS_DB_PASS + '@cluster0.baj5y.mongodb.net/Users?retryWrites=true&w=majority'
 }
